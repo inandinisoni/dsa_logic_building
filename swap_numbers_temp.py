16a9a6e0d8d1cca72a92cabb7fdfd976e77124b5
Nandini Soni <soninandini214@gmail.com>
2025-04-17 17:43:01 +0530

    Swap 2 numbers using 3rd variable(temp)
    Swapping 2 numbers using 3rd variable (temp)

---
+++ swap_numbers_temp.py
@@ -0,0 +1,7 @@
+a = int(input("enter a number "))
+b = int(input("enter a number "))
+print("Before Swap a = ", a, " and b = ", b)
+temp = a
+a = b
+b = temp
+print("After Swap a = ", a, " and b = ", b)
